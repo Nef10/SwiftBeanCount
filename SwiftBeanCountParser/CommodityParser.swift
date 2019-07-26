5e45ebb3f4c99a5f443f155a08a28e9201b5d99e
Steffen Kötte <steffen.koette@gmail.com>
2019-07-25 20:26:04 -0700

    Add Commodity Parser

---
+++ SwiftBeanCountParser/CommodityParser.swift
@@ -0,0 +1,34 @@
+//
+//  CommodityParser
+//  SwiftBeanCountParser
+//
+//  Created by Steffen Kötte on 2019-07-25.
+//  Copyright © 2019 Steffen Kötte. All rights reserved.
+//
+
+import Foundation
+import SwiftBeanCountModel
+
+struct CommodityParser {
+
+    static private let regex: NSRegularExpression = {
+        // swiftlint:disable:next force_try
+        try! NSRegularExpression(pattern: "^\(DateParser.dateGroup)\\s+commodity\\s+\(ParserUtils.commodityGroup)\\s*(;.*)?$", options: [])
+    }()
+
+    /// Parse commodity from a line String
+    ///
+    /// - Parameter line: String of one line
+    /// - Returns: commodity if the line could be parsed, otherwise nil
+    static func parseFrom(line: String) -> Commodity? {
+        let commodityMatches = line.matchingStrings(regex: self.regex)
+        guard
+            let match = commodityMatches[safe: 0],
+            let date = DateParser.parseFrom(string: match[1])
+        else {
+            return nil
+        }
+        return Commodity(symbol: match[2], opening: date)
+    }
+
+}
---
+++ SwiftBeanCountParserTests/CommodityParserTests.swift
@@ -0,0 +1,57 @@
+//
+//  CommodityParserTests.swift
+//  SwiftBeanCountParserTests
+//
+//  Created by Steffen Kötte on 2019-07-25.
+//  Copyright © 2019 Steffen Kötte. All rights reserved.
+//
+
+import SwiftBeanCountModel
+@testable import SwiftBeanCountParser
+import XCTest
+
+class CommodityParserTests: XCTestCase {
+
+    let basicString = "2017-06-09 commodity CAD"
+    let whitespaceString = "2017-06-09    commodity        CAD"
+    let endOfLineCommentString = "2017-06-09 commodity CAD ;gfsdt     "
+    let specialCharacterString = "2017-06-09 commodity CAD💵"
+    let invalidDateString = "2017-02-30 commodity CAD"
+
+    func testBasic() {
+        let commodity = CommodityParser.parseFrom(line: basicString)
+        XCTAssertEqual(commodity, Commodity(symbol: "CAD", opening: TestUtils.date20170609))
+    }
+
+    func testWhitespace() {
+        let commodity = CommodityParser.parseFrom(line: whitespaceString)
+        XCTAssertEqual(commodity, Commodity(symbol: "CAD", opening: TestUtils.date20170609))
+    }
+
+    func testEndOfLineComment() {
+        let commodity = CommodityParser.parseFrom(line: endOfLineCommentString)
+        XCTAssertEqual(commodity, Commodity(symbol: "CAD", opening: TestUtils.date20170609))
+    }
+
+    func testSpecialCharacter() {
+        let commodity = CommodityParser.parseFrom(line: specialCharacterString)
+        XCTAssertEqual(commodity, Commodity(symbol: "CAD💵", opening: TestUtils.date20170609))
+    }
+
+    func testInvalidCloseDate() {
+        let commodity = CommodityParser.parseFrom(line: invalidDateString)
+        XCTAssertNil(commodity)
+    }
+
+    func testPerformance() {
+        self.measure {
+            for _ in 0...1_000 {
+                _ = AccountParser.parseFrom(line: basicString)
+                _ = AccountParser.parseFrom(line: whitespaceString)
+                _ = AccountParser.parseFrom(line: endOfLineCommentString)
+                _ = AccountParser.parseFrom(line: specialCharacterString)
+            }
+        }
+    }
+
+}
