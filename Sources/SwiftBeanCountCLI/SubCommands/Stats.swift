7f87103be4e5546b9608d3d580d214990386d92d
Steffen Kötte <steffen.koette@gmail.com>
2020-06-13 01:26:36 -0700

    Make stats command formattable

--- Sources/SwiftBeanCountCLI/SubCommands/Stats.swift
+++ Sources/SwiftBeanCountCLI/SubCommands/Stats.swift
@@ -2,11 +2,12 @@ import ArgumentParser
 import Foundation
 import SwiftBeanCountModel
 
-struct Stats: LedgerCommand {
+struct Stats: FormattableLedgerCommand {
 
     static var configuration = CommandConfiguration(abstract: "Statistics of a ledger (e.g. # of transactions)")
 
     @OptionGroup() var options: LedgerOption
+    @ArgumentParser.Option(name: [.short, .long], default: .table, help: "Output format. \(Self.supportedFormats())") var format: Format
 
     func run() throws {
         let start = Date.timeIntervalSinceReferenceDate
@@ -14,19 +15,23 @@ struct Stats: LedgerCommand {
         let end = Date.timeIntervalSinceReferenceDate
         let time = end - start
 
-        print("""
-            \(ledger.transactions.count) Transactions
-            \(ledger.accounts.count) Accounts
-            \(ledger.accounts.filter { $0.opening != nil }.count) Account openings
-            \(ledger.accounts.filter { $0.closing != nil }.count) Account closings
-            \(ledger.tags.count) Tags
-            \(ledger.commodities.count) Commodities
-            \(ledger.events.count) Events
-            \(ledger.custom.count) Customs
-            \(ledger.option.count) Options
-            \(ledger.plugins.count) Plugins
-            \(String(format: "\nParsing time: %.3f sec", time))
-            """)
+        let values: [[String]] = [
+            ["Transactions", String(ledger.transactions.count)],
+            ["Accounts", String(ledger.accounts.count)],
+            ["Account openings", String(ledger.accounts.filter { $0.opening != nil }.count)],
+            ["Account closings", String(ledger.accounts.filter { $0.closing != nil }.count)],
+            ["Commodities", String(ledger.commodities.count)],
+            ["Tags", String(ledger.tags.count)],
+            ["Events", String(ledger.events.count)],
+            ["Customs", String(ledger.custom.count)],
+            ["Options", String(ledger.option.count)],
+            ["Plugins", String(ledger.plugins.count)]
+        ]
+
+        printFormatted(title: "Statistics", columns: ["Type", "Number"], values: values)
+        if format != .csv {
+            print(String(format: "\nParsing time: %.3f sec", time))
+        }
     }
 
 }
