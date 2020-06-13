23e555d6932109ae2baae830737d353ffccf3d97
Steffen Kötte <steffen.koette@gmail.com>
2020-06-13 01:25:56 -0700

    Adjust text format to show empty line after title

--- Sources/SwiftBeanCountCLI/LedgerCommand.swift
+++ Sources/SwiftBeanCountCLI/LedgerCommand.swift
@@ -42,22 +42,27 @@ extension FormattableLedgerCommand {
     func printFormatted(title: String, columns: [String], values: [[String]]) {
         var result: String
         switch format {
-        case .text, .table:
+        case .text:
+            var table = TextTable(columns: columns.map { TextTableColumn(header: $0) })
+            table.addRows(values: values)
+            table.columnFence = ""
+            table.rowFence = ""
+            table.cornerFence = ""
+            result = title + "\n\n"
+            result += table.render().split(whereSeparator: \.isNewline).map { $0.trimmingCharacters(in: .whitespaces) }.joined(separator: "\n")
+        case .table:
             var table = TextTable(columns: columns.map { TextTableColumn(header: $0) }, header: title)
             table.addRows(values: values)
-            if format == .text {
-                table.columnFence = ""
-                table.rowFence = ""
-                table.cornerFence = ""
-                result = table.render().split(whereSeparator: \.isNewline).map { $0.trimmingCharacters(in: .whitespaces) }.joined(separator: "\n")
-            } else {
-                result = table.render()
-            }
+            result = table.render()
         case .csv:
             result = columns.map { "\"\($0)\"" }.joined(separator: ", ") + "\n"
             result += values.map { $0.map { "\"\($0)\"" }.joined(separator: ", ") }.joined(separator: "\n")
         }
         print(result)
+    }
+
+    static func supportedFormats() -> String {
+        "Supported formats: \(Format.allCases.map { $0.rawValue }.joined(separator: ", "))"
     }
 
 }
