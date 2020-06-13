7a41288ffeeaad48d3c18b31e15393c0cd706649
Steffen Kötte <steffen.koette@gmail.com>
2020-06-13 00:01:31 -0700

    Add quiet flag to check command

--- Sources/SwiftBeanCountCLI/SubCommands/Check.swift
+++ Sources/SwiftBeanCountCLI/SubCommands/Check.swift
@@ -6,12 +6,16 @@ struct Check: LedgerCommand {
     static var configuration = CommandConfiguration(abstract: "Parses a ledger and prints any errors it finds")
 
     @OptionGroup() var options: LedgerOption
+    @ArgumentParser.Flag(name: [.short, .long], help: "Don't print errors, only indicate via exit code.") var quiet: Bool
+
 
     func run() throws {
         let ledger = try parseLedger()
         let errors = ledger.errors
         if !errors.isEmpty {
-            print(errors.joined(separator: "\n"))
+            if !quiet {
+                print(errors.joined(separator: "\n"))
+            }
             throw ExitCode(65)
         }
     }
