996f5f4ad10785e0da35fcd8cbd15e28f9633c28
Steffen Kötte <steffen.koette@gmail.com>
2020-07-07 23:29:23 -0700

    Update Model dependency to 0.1.1

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/Nef10/SwiftBeanCountModel.git",
-            .exact("0.1.0")
+            .exact("0.1.1")
         ),
     ],
     targets: [
