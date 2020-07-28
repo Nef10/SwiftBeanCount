c26a0b7292718e5c2cc81ef86ce5d15fc9cd771c
Steffen Kötte <steffen.koette@gmail.com>
2020-07-27 21:30:13 -0700

    Bump SwiftBeanCountModel version

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/Nef10/SwiftBeanCountModel.git",
-            .exact("0.1.2")
+            .exact("0.1.3")
         ),
     ],
     targets: [
