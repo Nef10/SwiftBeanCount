857eee4ab508776f90f019b82f5330a37805a1a1
swift-dependency-updater <unknown>
2021-09-10 10:34:39 +0000

    Bump swift-argument-parser from 0.4.4 to 0.5.0

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/apple/swift-argument-parser",
-            .upToNextMinor(from: "0.4.3")
+            .upToNextMinor(from: "0.5.0")
         ),
         .package(
             url: "https://github.com/scottrhoyt/SwiftyTextTable.git",
