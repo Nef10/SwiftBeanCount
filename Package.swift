2a5da576ad4e28c364575095f1bbcf0fa36d1f12
Steffen Kötte <steffen.koette@gmail.com>
2024-09-13 20:05:45 +0200

    Reset minimum required version for CSV

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/yaslab/CSV.swift.git",
-            from: "2.5.0"
+            from: "2.4.3"
         )
     ],
     targets: [
