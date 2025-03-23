b4a543ea13bae9a5b6fe5d04d16733c0f2d2377d
Steffen Kötte <steffen.koette@gmail.com>
2025-03-23 14:38:16 -0700

    Bump swiftlint to 0.57.0

--- Tests/SwiftBeanCountParserUtilsTests/StringTests.swift
+++ Tests/SwiftBeanCountParserUtilsTests/StringTests.swift
@@ -9,7 +9,7 @@
 @testable import SwiftBeanCountParserUtils
 import XCTest
 
-class StringTests: XCTestCase {
+final class StringTests: XCTestCase {
 
     func testMatchingStrings_multipleGroups() throws {
         let regex = try NSRegularExpression(pattern: "^\\s+([^\\s]+:[^\\s]+)\\s+(-?[0-9]+(.[0-9]+)?)\\s+([^\\s]+)\\s*(;.*)?$", options: [])
