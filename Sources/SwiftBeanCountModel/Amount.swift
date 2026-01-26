e84315919d4a025e544da2bce4a46571b3880909
Steffen Kötte <steffen.koette@gmail.com>
2026-01-25 23:02:32 -0800

    restore log message

--- Sources/SwiftBeanCountModel/Amount.swift
+++ Sources/SwiftBeanCountModel/Amount.swift
@@ -67,7 +67,8 @@ extension Amount: CustomStringConvertible {
             return min
         }
 
-        fatalError("Unable to format amountString for number: \(number) with decimalDigits: \(decimalDigits)")
+        fatalError("Unable to format amountString for number: \(number) with decimalDigits: \(decimalDigits). " +
+            "Min decimal digits: \(minDecimalDigits), Max decimal digits: \(maxDecimalDigits)")
 #endif
     }
 
