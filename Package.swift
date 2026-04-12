410b576d9e53d3c9186c89a98635afa325bac97a
Steffen Kötte <steffen.koette@gmail.com>
2026-04-11 21:15:03 -0700

    Tweak skip warning
    Locally works now with Xcode 26.4, but GitHub actions even with the
    same version still has problems building an app consuming this package.
    
    Will try this and set the Environment Variable in the app build

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ var swiftSettings: [SwiftSetting] = [
 ]
 
 // Workaround for https://forums.swift.org/t/warnings-as-errors-in-sub-packages/70810/24
-if ProcessInfo.processInfo.environment["CI"] == "true" {
+if ProcessInfo.processInfo.environment["SKIP_WARNINGS_SWIFTBEANCOUNT"] != "true" {
     swiftSettings.append(contentsOf: [
        .treatAllWarnings(as: .error),
        .treatWarning("SendableClosureCaptures", as: .warning),
