7ee071d95db9bba07e0d75e5e69096e45755ca87
Steffen Kötte <steffen.koette@gmail.com>
2026-08-11 23:45:33 -0700

    Migrate some tests from XCTests to Swift Testing

--- Tests/TangerineDownloaderTests/TangerineDownloaderErrorTests.swift
+++ Tests/TangerineDownloaderTests/TangerineDownloaderErrorTests.swift
@@ -1,17 +1,11 @@
 @testable import TangerineDownloader
-import XCTest
+import Testing
 
-final class TangerineDownloaderErrorTests: XCTestCase {
-
-    func testDownloadErrorString() {
-         XCTAssertEqual(
-            "\(TangerineDownloaderError.accountsLoadingFailed.localizedDescription)",
-            "Could not parse the accounts from the server"
-        )
-         XCTAssertEqual(
-            "\(TangerineDownloaderError.transactionLoadingFailed.localizedDescription)",
-            "Could not parse the transactions from the server"
-        )
-    }
-
+@Suite
+struct TangerineDownloaderErrorTests {
+   @Test
+   func downloadErrorString() {
+        #expect("\(TangerineDownloaderError.accountsLoadingFailed.localizedDescription)" == "Could not parse the accounts from the server")
+        #expect("\(TangerineDownloaderError.transactionLoadingFailed.localizedDescription)" == "Could not parse the transactions from the server")
+   }
 }
