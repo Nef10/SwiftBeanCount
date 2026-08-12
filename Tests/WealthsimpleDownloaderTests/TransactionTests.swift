02f19e80d5b4a533da0c72f2acbf664835eff775
Steffen Kötte <steffen.koette@gmail.com>
2026-08-11 23:48:52 -0700

    Fix whitespace

--- Tests/WealthsimpleDownloaderTests/TransactionTests.swift
+++ Tests/WealthsimpleDownloaderTests/TransactionTests.swift
@@ -18,11 +18,13 @@ final class TransactionTests {
        let noDataReceived = TransactionError.noDataReceived
        #expect(noDataReceived == .noDataReceived)
        #expect(TransactionError.noDataReceived != TransactionError.invalidParameter)
+
        // Test .httpError
        let httpError = TransactionError.httpError(error: "test")
        #expect(httpError == .httpError(error: "test"))
        #expect(TransactionError.httpError(error: "test") != TransactionError.httpError(error: "different"))
        #expect(TransactionError.httpError(error: "test") != TransactionError.noDataReceived)
+
        // Test .invalidJson
        let data1 = Data("test1".utf8)
        let data2 = Data("test2".utf8)
@@ -36,11 +38,13 @@ final class TransactionTests {
    func transactionErrorEqualityJSONParameters() {
        let json1 = ["key": "value" as Any]
        let json2 = ["key": "other" as Any]
+
        // Test .missingResultParameter
        let missingResult = TransactionError.missingResultParameter(json: json1)
        #expect(missingResult == .missingResultParameter(json: json1))
        #expect(TransactionError.missingResultParameter(json: json1) != TransactionError.missingResultParameter(json: json2))
        #expect(TransactionError.missingResultParameter(json: json1) != TransactionError.noDataReceived)
+
        // Test .invalidResultParameter
        let invalidResult = TransactionError.invalidResultParameter(json: json1)
        #expect(invalidResult == .invalidResultParameter(json: json1))
@@ -55,6 +59,7 @@ final class TransactionTests {
        #expect(tokenError == .tokenError(.noToken))
        #expect(TransactionError.tokenError(.noToken) != TransactionError.tokenError(.invalidJson(error: "test")))
        #expect(TransactionError.tokenError(.noToken) != TransactionError.noDataReceived)
+
        // Test .invalidParameter
        let invalidParameter = TransactionError.invalidParameter
        #expect(invalidParameter == .invalidParameter)
